8678ee158419ea18c347cb45556f7e546a9d1e40
Jang SuHyeon <jshh020327@gmail.com>
2026-06-10 00:47:39 +0900

    feat: initialize mini hospital system project

---
+++ src/hospital/Patient.java
@@ -0,0 +1,19 @@
+package hospital;
+
+public class Patient {
+    private String name;
+    private int age;
+
+    public Patient(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
